43f284ea0c63be441df129704dbb56f29a821519
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-29 09:39:39 -0500

    Add Weapon

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Reflection;
+
+public class WeaponController: MonoBehaviour {
+
+  public Transform weaponHold;
+  public Weapon initialWeapon;
+
+  Weapon weapon;
+
+  void Start() {
+    if (initialWeapon != null) {
+      EquipWeapon(initialWeapon);
+    }
+  }
+
+  public void EquipWeapon(Weapon aWeapon) {
+    if (weapon != null) {
+      // Mark the current weapon for destruction
+      Destroy(weapon);
+    }
+
+    weapon = Instantiate(aWeapon, weaponHold.position, weaponHold.rotation) as Weapon;
+    // Bind the weapon as a child of the  to the `weaponHold` game object, which is a child of
+    // player, so that when the player moves the gun moves also
+    weapon.transform.parent = weaponHold;
+  }
+}
