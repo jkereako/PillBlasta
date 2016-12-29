210e883d04c8c45794cf7b57a83a1e7528dd0db0
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-29 00:05:07 -0500

    Add the player and player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+// Declare the file's dependencies
+[RequireComponent(typeof(PlayerController))]
+public class Player: MonoBehaviour {
+  public float speed = 5;
+
+  PlayerController controller;
+  Camera camera;
+
+  void Start() {
+    controller = GetComponent<PlayerController>();
+    camera = Camera.main;
+  }
+
+  void Update() {
+    // `GetAxisRaw` returns the unmolested input value. We use it here to make the pill stop on a dime.
+    Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    Vector3 velocity = movement.normalized * speed;
+    controller.Move(velocity);
+
+    // The code below allows the player's "eyes" to follow the mouse movement.
+    Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+    // Emulate the playing surface by creating a new, flat plane. This simplifies the code as we
+    // don't have to depend on the actual in-game plane.
+    Plane plane = new Plane(Vector3.up, Vector3.zero);
+    float rayLength;
+
+    if (plane.Raycast(ray, out rayLength)) {
+      Vector3 point = ray.GetPoint(rayLength);
+      controller.LookAt(point);
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController: MonoBehaviour {
+  Rigidbody rigidBody;
+  Vector3 velocity;
+
+  void Start() {
+    rigidBody = GetComponent<Rigidbody>();
+  }
+
+  void FixedUpdate() {
+    rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+  }
+
+  public void Move(Vector3 aVelocity) {
+    velocity = aVelocity;
+  }
+
+  public void LookAt(Vector3 point) {
+    // The incoming point is not relative to the player's height. The line below corrects the point
+    // so that it is at eye-level with the player.
+    Vector3 correctedPoint = new Vector3(point.x, transform.position.y, point.z);
+    transform.LookAt(correctedPoint);
+  }
+}
