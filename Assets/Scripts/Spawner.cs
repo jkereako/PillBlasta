f6d64b25e82e3cc52bf8140ec9a10fff97c9921c
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-30 09:16:07 -0500

    Shorten gun, add spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Spawner: MonoBehaviour {
+  public Enemy enemy;
+  public Wave[] waves;
+
+  Wave currentWave;
+  int waveIndex;
+  int enemiesRemaining;
+  float nextSpawnTime;
+
+  void Start() {
+    NextWave();
+  }
+
+  void Update() {
+    if (enemiesRemaining > 0 && Time.time > nextSpawnTime) {
+      enemiesRemaining -= 1;
+      nextSpawnTime = Time.time + currentWave.waitValue;
+
+      Enemy spawn = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+    }
+  }
+
+  void NextWave() {
+    waveIndex += 1;
+    currentWave = waves[waveIndex - 1];
+
+    enemiesRemaining = currentWave.enemyCount;
+  }
+
+  [System.Serializable]
+  public class Wave {
+    public int enemyCount;
+    public float waitValue;
+  }
+
+}
