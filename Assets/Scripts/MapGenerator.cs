427cc67abcbc7c1ad7b83f2a70aa9c9b05539e38
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-31 12:58:18 -0500

    Add algorithm to prevent inaccessible paths

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,8 +7,11 @@ public class MapGenerator: MonoBehaviour {
   public Vector2 mapSize;
   [Range(0, 1)]
   public float outLinePercent;
+  [Range(0, 1)]
+  public float obstaclePercent;
   List<Coordinate> tileCoordinates;
   Queue<Coordinate> shuffledTileCoordinates;
+  Coordinate mapCenter;
   public int seed = 10;
 
   public struct Coordinate {
@@ -18,6 +21,14 @@ public class MapGenerator: MonoBehaviour {
     public Coordinate(int xCoordinate, int yCoordinate) {
       x = xCoordinate;
       y = yCoordinate;
+    }
+
+    public static bool operator ==(Coordinate c1, Coordinate  c2) {
+      return c1.x == c2.x && c1.y == c2.y;
+    }
+
+    public static bool operator !=(Coordinate c1, Coordinate  c2) {
+      return c1.x != c2.x || c1.y != c2.y;
     }
   }
 
@@ -35,7 +46,9 @@ public class MapGenerator: MonoBehaviour {
       }
     }
 
+    mapCenter = new Coordinate((int)mapSize.x / 2, (int)mapSize.y / 2);
     shuffledTileCoordinates = new Queue<Coordinate>(Utility.Shuffle(tileCoordinates.ToArray(), seed));
+
     // First, immediately destroy the container object if it already exists.
     if (transform.FindChild(containerName)) {
       DestroyImmediate(transform.FindChild(containerName).gameObject);
@@ -45,6 +58,7 @@ public class MapGenerator: MonoBehaviour {
     Transform containerObject = new GameObject(containerName).transform;
     containerObject.parent = transform;
 
+    // Generate the tiles
     for (int x = 0; x < mapSize.x; x++) {
       for (int y = 0; y < mapSize.y; y++) {
         Vector3 tilePosition = CoordinateToPosition(x, y);
@@ -55,14 +69,62 @@ public class MapGenerator: MonoBehaviour {
       }
     }
 
-    const int obstacleCount = 10;
-
+    bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+    int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent);
+    int currentObstacleCount = 0;
+    // Generate the obstalces
     for (int i = 0; i < obstacleCount; i++) {
       Coordinate coordinate = GetRandomCoordinate();
+      obstacleMap[coordinate.x, coordinate.y] = true;
+      currentObstacleCount += 1;
+
+      if (coordinate == mapCenter || !IsMapCompletelyAccessible(obstacleMap, i)) {
+        currentObstacleCount -= 1;
+        obstacleMap[coordinate.x, coordinate.y] = false;
+        continue;
+      }
+
       Vector3 obstaclePosition = CoordinateToPosition(coordinate.x, coordinate.y);
       Transform obstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
       obstacle.parent = containerObject;
     }
+  }
+
+  // This is an implementation of the Flood-fill 4 algorithm.
+  bool IsMapCompletelyAccessible(bool[,] obstacleMap, int obstacleCount) {
+    bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+    Queue<Coordinate> queue = new Queue<Coordinate>();
+
+    queue.Enqueue(mapCenter);
+    mapFlags[mapCenter.x, mapCenter.y] = true;
+    int accessibleTileCount = 0;
+
+    while (queue.Count > 0) {
+      Coordinate tile = queue.Dequeue();
+
+      for (int x = -1; x < 2; x++) {
+        for (int y = -1; y < 2; y++) {
+          int neighborX = tile.x + x;
+          int neighborY = tile.y + y;
+
+          if (x == 0 || y == 0) {
+            if (neighborX >= 0 && neighborX < obstacleMap.GetLength(0) &&
+                neighborY >= 0 && neighborY < obstacleMap.GetLength(1)) {
+
+              if (!mapFlags[neighborX, neighborY] && !obstacleMap[neighborX, neighborY]) {
+                mapFlags[neighborX, neighborY] = true;
+                queue.Enqueue(new Coordinate(neighborX, neighborY));
+                accessibleTileCount += 1;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    int expectedAccessibleTileCount = (int)(mapSize.x * mapSize.y - obstacleCount);
+
+    return expectedAccessibleTileCount == accessibleTileCount;
   }
 
   Vector3 CoordinateToPosition(int x, int y) {
