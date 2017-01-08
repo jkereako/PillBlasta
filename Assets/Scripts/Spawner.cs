4684c1c5952e43bad971068cc730afbecd8dffe3
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-01-08 11:05:05 -0500

    Simplify conditions

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ public class Spawner: MonoBehaviour {
   public Wave[] waves;
 
   Wave currentWave;
-  int waveIndex;
+  int waveIndex = 0;
   int spawnCount;
   int activeCount;
   float nextSpawnTime;
@@ -15,30 +15,28 @@ public class Spawner: MonoBehaviour {
   }
 
   void Update() {
-    if (spawnCount > 0 && Time.time > nextSpawnTime) {
-      spawnCount -= 1;
-      nextSpawnTime = Time.time + currentWave.waitValue;
-
-      Enemy spawn = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
-      // Assign the delegate `OnEnemyDeath`
-      spawn.OnDeath += OnEnemyDeath;
+    if (spawnCount <= 0 || Time.time < nextSpawnTime) {
+      return;
     }
+
+    spawnCount -= 1;
+    nextSpawnTime = Time.time + currentWave.waitValue;
+
+    Enemy spawn = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+    // Assign the delegate `OnEnemyDeath`
+    spawn.OnDeath += OnEnemyDeath;
   }
 
   void OnEnemyDeath() {
     activeCount -= 1;
 
-    if (activeCount == 0) {
+    if (activeCount == 0 && waveIndex < waves.Length) {
       NextWave();
     }
   }
 
   void NextWave() {
     Debug.Log("Wave: " + waveIndex);
-
-    if (waveIndex > waves.Length) {
-      return;
-    }
 
     currentWave = waves[waveIndex];
     activeCount = spawnCount = currentWave.entityCount;
