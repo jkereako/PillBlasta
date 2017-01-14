8d4b39afe53c13334c81ab87da1f60b0bb2eef21
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-01-14 14:51:44 -0500

    Add flash animation

---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MuzzleFlash: MonoBehaviour {
+  public GameObject flash;
+  public Sprite[] sprites;
+  public SpriteRenderer[] spriteRenderers;
+  public float flashTime;
+
+  void Start() {
+    Deactivate();
+  }
+
+  public void Activate() {
+    int spriteIndex = Random.Range(0, sprites.Length);
+
+    for (int i = 0; i < spriteRenderers.Length; i++) {
+      spriteRenderers[i].sprite = sprites[spriteIndex];
+    }
+
+    flash.SetActive(true);
+
+    Invoke("Deactivate", flashTime);
+  }
+
+  public void Deactivate() {
+    flash.SetActive(false);
+  }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(MuzzleFlash))]
 public class Weapon: MonoBehaviour {
   public Transform muzzle;
   public Transform ejector;
   public Projectile projectile;
   public Transform shell;
+
   public float fireRate = 100;
   public float muzzleVelocity = 35;
 
   float nextShotTime;
+  MuzzleFlash muzzleFlash;
+
+  public void Start() {
+    muzzleFlash = GetComponent<MuzzleFlash>();
+  }
 
   public void Fire() {
     if (Time.time < nextShotTime) {
@@ -22,5 +29,6 @@ public class Weapon: MonoBehaviour {
     aProjectile.SetSpeed(muzzleVelocity);
 
     Instantiate(shell, ejector.position, ejector.rotation);
+    muzzleFlash.Activate();
   }
 }
