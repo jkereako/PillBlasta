560ed0f4b734262b963086a9fe50e052c3c6c4e7
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-01-19 20:37:02 -0500

    Fix aiming bug

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,13 +21,10 @@ public class Player: LiveEntity {
   }
 
   void Update() {
-    // `GetAxisRaw` returns the unmolested input value. We use it here to make the pill stop on a dime.
-    Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-    Vector3 velocity = movement.normalized * trait.locomotiveSpeed;
-    playerController.Move(velocity);
-
     // The code below allows the player's "eyes" to follow the mouse movement.
     Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+    //  Debug.DrawLine(ray.origin, point, Color.red);
+
     // Emulate the playing surface by creating a new, flat plane. This simplifies the code as we
     // don't have to depend on the actual in-game plane.
     //
@@ -35,14 +32,26 @@ public class Player: LiveEntity {
     Plane plane = new Plane(Vector3.up, Vector3.up * weaponController.weaponHold.position.y);
     float rayLength;
 
-    if (plane.Raycast(ray, out rayLength)) {
-      Vector3 point = ray.GetPoint(rayLength);
-//      Debug.DrawLine(ray.origin, point, Color.red);
-      playerController.LookAt(point);
-      weaponController.Aim(point);
-      crossHair.transform.position = point;
-      crossHair.DetectTargets(ray);
+    // This function sets enter to the distance along the ray, where it intersects the plane. If the
+    // ray is parallel or pointing in the opposite direction to the plane then `Raycast()` is false
+    // and `rayLength` is 0 and negative, respectively.
+    if (!plane.Raycast(ray, out rayLength)) {
+      return;
     }
+     
+    const int threshold = 6;
+    Vector3 point = ray.GetPoint(rayLength);
+    Vector2 newPoint = new Vector2(point.x, point.z);
+    Vector2 newPosition = new Vector2(transform.position.x, transform.position.z);
+
+    playerController.LookAt(point);
+         
+    if ((newPoint - newPosition).sqrMagnitude > threshold) {
+      weaponController.Aim(point);
+    }
+
+    crossHair.transform.position = point;
+    crossHair.DetectTargets(ray);
 
     if (Input.GetMouseButton(0)) {
       weaponController.OnTriggerPull();
@@ -50,5 +59,15 @@ public class Player: LiveEntity {
     else if (Input.GetMouseButtonUp(0)) {
       weaponController.OnTriggerRelease();
     }
+
+    Move();
+  }
+
+  void Move() {
+    // `GetAxisRaw` returns the unmolested input value. We use it here to make the pill stop on a 
+    // dime.
+    Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    Vector3 velocity = movement.normalized * trait.locomotiveSpeed;
+    playerController.Move(velocity);
   }
 }
